af1828edc793af69ba6315bca373068f51b0f7b0
unknown <jeffreymaestre@gmail.com>
2023-10-27 13:13:21 -0500

    cargue inicial de archivos al repositorio

---
+++ src/main/java/taxislibres/jeffrey/maestre/MaestreApplication.java
@@ -0,0 +1,13 @@
+package taxislibres.jeffrey.maestre;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaestreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaestreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/taxislibres/jeffrey/maestre/MaestreApplicationTests.java
@@ -0,0 +1,13 @@
+package taxislibres.jeffrey.maestre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaestreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
